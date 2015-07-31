0cab2827a87d4d5b2dcfbdbf8a296b99551a246b
George <gg@localhost.localdomain>
2015-07-31 00:53:15 +0000

    template if number

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 
 console.log("this is both sides.");
+// global(Client & Server) not var
+Message = new Mongo.Collection("message");
+
+sampleMessages = [
+	{text: "Hi Meteor! (m1)"},
+	{text: "Agilearning.io is awesome! (m2)"},
+	{text: "Agilearning.io is cute (m3)"} 
+]
+ 
 
 if(Meteor.isClient){
   console.log("this is client!");
  
- Template.message.helpers({
-  	test:"test123"
- })
 
   Template.body.helpers({
-  	testArray: _.range(0,10),
-  	testObject:{
-  		test:"test1234"
-  	},
-  	testDate: new Date,
-  	Msgs:[
-  		{n:1,text: "Hi Meteor! (m1)"},
-  		{n:2,text: "Agilearning.io is awesome! (m2)"},
-  		{n:3,text: "Agilearning.io is cute (m3)"} 
-  	]
+  	Msgs : sampleMessages //[
+// 	{n:1, text: "Hi Meteor! (m1)"},
+// 	{n:2, text: "Agilearning.io is awesome! (m2)"},
+// 	{n:3, text: "Agilearning.io is cute (m3)"} 
+// ]
   })
 
 }
@@ -26,5 +27,10 @@ if(Meteor.isClient){
 if(Meteor.isServer){
   console.log("this is server");
 }
+
+
+
+
+
 
 
