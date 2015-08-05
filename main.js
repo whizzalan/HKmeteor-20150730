0ec96d18a6c0acbd88c56d849cf37382a12e73c0
GeorgeChao <whizzalan@gmail.com>
2015-08-05 16:05:26 +0000

    modify msgData can't show with text

--- main.js
+++ main.js
@@ -1,63 +1,28 @@
-
-console.log("this is both sides.");
 // global(Client & Server) not var
 Message = new Mongo.Collection("message");
-
-sampleMessages = [
-	{text: "Hi Meteor! (m1)", from :"db"},
-	{text: "Agilearning.io is awesome! (m2)"},
-	{text: "Agilearning.io is cute (m3)"} 
-]
  
 
 if(Meteor.isClient){
-  console.log("this is client!");
- 
-
-  Template.body.helpers({
+  
+ Template.body.helpers({
   	// from data 
   	Msgs: function(){
   		return(Message.find())
   	}
-  	// Msgs : sampleMessages //[
-// 	{n:1, text: "Hi Meteor! (m1)"},
-// 	{n:2, text: "Agilearning.io is awesome! (m2)"},
-// 	{n:3, text: "Agilearning.io is cute (m3)"} 
-// ]
   })
   // print input
   Template.body.events({
   	// e:event, t:template
   	"change #inputMsg": function(e,t){
-  		console.log($(e.target).val());
-  		console.log("change #inputMsg");
   		msg = $(e.target).val();
   		// after message and clear space
   		$(e.target).val("");
   		msgData = {
-  			test:msg,
+  			text:msg,
   			createdAt: new Date,
   		};
-  		console.log(msgData);
   		Message.insert(msgData);
   	}
   })
 
 }
-
-if(Meteor.isServer){
-  console.log("this is server");
-  // check data
-  if (Message.find().count() === 0){
-  	for (i in sampleMessages){
-  		Message.insert(sampleMessages[i])
-  	}
-  }
-}
-
-
-
-
-
-
-
